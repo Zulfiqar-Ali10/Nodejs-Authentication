2d8cea782cefbd1ae3e0387388f199c7a76821c8
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-01-26 14:14:19 +0500

    changes

--- app.js
+++ app.js
@@ -1,47 +1,60 @@
-import express from 'express';
-import route from './routes/user-routes.js';
-import cors from 'cors';
-import {connectDb} from './config/config.js';
-import pkg from 'body-parser';
-const { json } = pkg;
-import dotenv from 'dotenv';
-import mongoose from "mongoose";
+// import express from 'express';
+// import route from './routes/user-routes.js';
+// import cors from 'cors';
+// import {connectDb} from './config/config.js';
+// import pkg from 'body-parser';
+// const { json } = pkg;
+// import dotenv from 'dotenv';
+// import mongoose from "mongoose";
 
 
 
+// const app = express();
+
+// dotenv.config(); 
+
+// app.use(json());
+
+// console.log("MONGO_URI", process.env.MONGO_URI);
+
+
+// connectDb();
+
+//  // Allow all origins
+// const corsOption = {
+//     origin: 'https://hackathons-sigma-flame.vercel.app',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     CredentialS: true,
+//     optionSuccessStatus: 204
+// };
+// app.use(cors(corsOption));
+
+// app.use('/api',route);
+
+// app.get('/', (req, res) => {
+//     console.log("req=>" , req);
+//     res.send("Hello world");
+// })
+// mongoose.connect(process.env.MONGO_URI)
+// .then(()=> console.log("Mongodb Connect"))
+// .catch((err)=> console.log("err", err));
+
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+import express from "express";
 const app = express();
+const port = process.env.PORT || 5000;
 
-dotenv.config(); 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.use(json());
-
-console.log("MONGO_URI", process.env.MONGO_URI);
-
-
-connectDb();
-
- // Allow all origins
-const corsOption = {
-    origin: 'https://hackathons-sigma-flame.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    CredentialS: true,
-    optionSuccessStatus: 204
-};
-app.use(cors(corsOption));
-
-app.use('/api',route);
-
-app.get('/', (req, res) => {
-    console.log("req=>" , req);
-    res.send("Hello world");
-})
-mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("Mongodb Connect"))
-.catch((err)=> console.log("err", err));
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
